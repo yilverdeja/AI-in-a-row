f9110315efd71764c6de70e67491df53eb2c9a27
yilverdeja <yilverdeja@gmail.com>
2020-11-14 02:12:59 +0800

    human player moves

--- gomoku/game.py
+++ gomoku/game.py
@@ -1,4 +1,5 @@
 import math
+from player import HumanPlayer
 
 BOARD_SIZE = 15
 
@@ -11,15 +12,16 @@ class Game():
     # Makes the 15x15 board to play Gomoku
     @staticmethod
     def makeBoard():
-        return [[None for j in range(BOARD_SIZE)] for i in range(BOARD_SIZE)]
+        return [[" " for j in range(BOARD_SIZE)] for i in range(BOARD_SIZE)]
     
     # prints a 15x15 board
     def printBoard(self):
-        pass
+        for i in range(BOARD_SIZE):
+            print('| ' + ' | '.join(self.board[i]) + ' |')
                 
     # Attempts to make a move on the board
     def makeMove(self, coord, letter):
-        if (self.board[coord[0]][coord[1]] == None):
+        if (self.board[coord[0]][coord[1]] == " "):
             self.board[coord[0]][coord[1]] = letter
             # TODO: check winner?
             return True
@@ -28,26 +30,37 @@ class Game():
     # Attempts to undo a move on the board
     def undoMove(self, coord, letter):
         if (self.board[coord[0]][coord[1]] == letter):
-            self.board[coord[0]][coord[1]] = None
+            self.board[coord[0]][coord[1]] = " "
             if self.winner != None:
                 self.winner = None
             return True
         return False
     
 # plays the game
-def play(game):
+def play(game, playerX, playerY):
     game.printBoard()
-    # coord = (0,0)
-    # if game.makeMove(coord, "X"):
-    #     print("move success")
-    # print(game.board)
-    # if game.undoMove(coord, "X"):
-    #     print("undo success")
-    # print(game.board)
+
+    letter = "X"
+    
+    # TODO: or no more available coords
+    while game.winner == None:
+        print("Player "+letter+" move")
+        if letter == "X":
+            playerX.makeMove(game)
+        else:
+            playerO.makeMove(game)
+        
+        game.printBoard()
+
+        # swap player
+        letter = "X" if letter == "O" else "O"
+
     return
 
 if __name__ == "__main__":
     g = Game()
-    play(g)
+    playerX = HumanPlayer("X")
+    playerO = HumanPlayer("O")
+    play(g, playerX, playerO)
     
     
--- gomoku/player.py
+++ gomoku/player.py
@@ -13,7 +13,24 @@ class HumanPlayer(Player):
         super().__init__(letter)
     
     def makeMove(self, game):
+        while True:
+            rowPos = self.getPosition("row")
+            colPos = self.getPosition("col")
+
+            if game.makeMove((rowPos, colPos), self.letter):
+                break
+            else:
+                print("Must choose an empty position on the board!")
+
         return
+
+    def getPosition(self, posType):
+        while True:
+            pos = int(input(posType+" (0-14): "))
+            if pos >= 0 and pos <= 14:
+                return pos
+            else:
+                print("Must choose a position between 0 and 14")
 
 class AIPlayer(Player):
     def __init__(self, letter):
