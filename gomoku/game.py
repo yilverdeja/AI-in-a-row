2ad9cbd0bda6855b44e0d53fea4b312271d6f40f
yilverdeja <yilverdeja@gmail.com>
2020-11-13 19:56:45 +0800

    board, make and undo

--- gomoku/game.py
+++ gomoku/game.py
@@ -11,25 +11,43 @@ class Game():
     # Makes the 15x15 board to play Gomoku
     @staticmethod
     def makeBoard():
-        return 1
+        return [[None for j in range(BOARD_SIZE)] for i in range(BOARD_SIZE)]
     
     # prints a 15x15 board
     def printBoard(self):
-        return
+        pass
+                
+    # Attempts to make a move on the board
+    def makeMove(self, coord, letter):
+        if (self.board[coord[0]][coord[1]] == None):
+            self.board[coord[0]][coord[1]] = letter
+            # TODO: check winner?
+            return True
+        return False
     
-    # Makes a move on the board
-    def makeMove(self):
-        return
-    
-    # Undoes a move, useful when simulating the game
-    def undoMove(self):
-        return
+    # Attempts to undo a move on the board
+    def undoMove(self, coord, letter):
+        if (self.board[coord[0]][coord[1]] == letter):
+            self.board[coord[0]][coord[1]] = None
+            if self.winner != None:
+                self.winner = None
+            return True
+        return False
     
 # plays the game
-def play(self):
+def play(game):
+    game.printBoard()
+    # coord = (0,0)
+    # if game.makeMove(coord, "X"):
+    #     print("move success")
+    # print(game.board)
+    # if game.undoMove(coord, "X"):
+    #     print("undo success")
+    # print(game.board)
     return
 
 if __name__ == "__main__":
-    pass
+    g = Game()
+    play(g)
     
     
