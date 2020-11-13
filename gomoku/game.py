6285e649496f02716359f666747e48ce3794f11a
yilverdeja <yilverdeja@gmail.com>
2020-11-14 04:32:17 +0800

    so much left to go

--- gomoku/game.py
+++ gomoku/game.py
@@ -1,7 +1,15 @@
 import math
-from player import HumanPlayer
+from player import HumanPlayer, AIPlayer
 
+# TODO: don't hardcode in player.py
 BOARD_SIZE = 15
+
+moves = {"X": [], "O": []}
+
+# Shows all open "in a row moves". Every dictionary holds in array of tuple arrays i.e [[(), ()], [(), (), ()]]
+# c# is a closed combo which means one side is blocked and you only need one play to block it completely
+xMoves = {"4": [], "c4": [], "3": [], "c3": [], "2": [], "c2": [], "blocked": []}
+oMoves = {"4": [], "c4": [], "3": [], "c3": [], "2": [], "c2": [], "blocked": []}
 
 class Game():
 
@@ -36,6 +44,72 @@ class Game():
             return True
         return False
     
+    def isBoardEmpty(self):
+        return len(moves["X"]) == 0 and len(moves["O"]) == 0
+    
+    def isBoardFull(self):
+        return len(moves["X"]) + len(moves["O"]) >= int(math.pow(BOARD_SIZE, 2))
+    
+    def getNumPosLeft(self):
+        return int(math.pow(BOARD_SIZE, 2)) - len(moves["X"]) - len(moves["O"])
+    
+    def getPlayerScore(self, letter):
+        # TODO is there a better way to simplify this
+        if letter == "X":
+            return 1000*len(xMoves["4"]) + 100*len(xMoves["c4"]) + 50*len(xMoves["3"]) + 25*len(xMoves["c3"]) + 10*len(xMoves["2"]) + 5*len(xMoves["c2"])
+        elif letter == "O":
+            return 1000*len(oMoves["4"]) + 100*len(oMoves["c4"]) + 50*len(oMoves["3"]) + 25*len(oMoves["c3"]) + 10*len(oMoves["2"]) + 5*len(oMoves["c2"])
+    
+    # TODO
+    def updateXMoves(self, pos):
+        # look through all current xMoves and check next to their rows if pos exists there. If so, then remove it from the in-a-row dic value and add it to the next one
+        for item in xMoves.items():
+            # TODO add if block into helper function to be used by updateOMoves
+            if len(item[0]) > 0:
+                if item[0] == "c2":
+                    # [[(0, 1), (0, 2)], [(4,4), (5,4)]], 
+                    # for first one it searches if pos is at (0,0), (0,3) or (0,4)
+                    # for second one it searches if pos is at (2, 4), (3, 4), (6,4), (7,4)
+                    # if it is, then it removes this item from c2 and adds it into c3
+                    pass
+                elif item[0] == "2":
+                    pass
+                elif item[0] == "c3":
+                    pass
+                elif item[0] == "3":
+                    pass
+                elif item[0] == "c4":
+                    pass
+                elif item[0] == "4":
+                    pass
+                else:
+                    raise Exception("Shouldn't arrive here!")
+        pass
+
+    def updateOMoves(self, pos):
+        pass
+
+    def getPotentialMoves(self, dist):
+        moveSet = set()
+        if dist == 1:
+            vectors = [(1, 0), (0, 1), (1,1), (1,-1)]
+        else:
+            vectors = [(-1, 0), (0, -1), (-1,-1), (1,-1), (2, 1), (2, 0), (2, -1), (2, -2), (1, -2), (0, -2), (-1, -2), (-2, -2)]
+        
+        allMoves = moves["X"] + moves["O"]
+
+        for move in allMoves:
+            for vector in vectors:
+                head = (move[0] + vector[0], move[1] + vector[1])
+                tail = (move[0] - vector[0], move[1] + vector[1])
+                if not self.isOutOfRange(head) and self.board[head[0]][head[1]] == " ":
+                    moveSet.add(head)
+                if not self.isOutOfRange(tail) and self.board[tail[0]][tail[1]] == " ":
+                    moveSet.add(tail)
+
+    def isOutOfRange(self, pos):
+        return pos[0] < 0 or pos[1] < 0 or pos[0] > BOARD_SIZE-1 or pos[1] > BOARD_SIZE-1 
+    
 # plays the game
 def play(game, playerX, playerY):
     game.printBoard()
@@ -46,11 +120,16 @@ def play(game, playerX, playerY):
     while game.winner == None:
         print("Player "+letter+" move")
         if letter == "X":
-            playerX.makeMove(game)
+            pos = playerX.makeMove(game)
+            moves["X"].append(pos)
+            game.updateXMoves(pos)
         else:
-            playerO.makeMove(game)
+            pos = playerO.makeMove(game)
+            moves["O"].append(pos)
+            game.updateOMoves(pos)
         
         game.printBoard()
+        print(moves)
 
         # swap player
         letter = "X" if letter == "O" else "O"
@@ -59,7 +138,7 @@ def play(game, playerX, playerY):
 
 if __name__ == "__main__":
     g = Game()
-    playerX = HumanPlayer("X")
+    playerX = AIPlayer("X")
     playerO = HumanPlayer("O")
     play(g, playerX, playerO)
     
--- gomoku/player.py
+++ gomoku/player.py
@@ -18,11 +18,9 @@ class HumanPlayer(Player):
             colPos = self.getPosition("col")
 
             if game.makeMove((rowPos, colPos), self.letter):
-                break
+                return (rowPos, colPos)
             else:
                 print("Must choose an empty position on the board!")
-
-        return
 
     def getPosition(self, posType):
         while True:
@@ -37,11 +35,56 @@ class AIPlayer(Player):
         super().__init__(letter)
     
     def makeMove(self, game):
+        if game.isBoardEmpty():
+            # place randomly in center
+            pos = random.choice([(6,6),(6,7),(6,8),(7,6),(7,7),(7,8),(8,6),(8,7),(8,8)])
+            if game.makeMove(pos, self.letter):
+                return pos
+            else:
+                raise Exception("Something is wrong. You should be able to make a move if the board is empty...")
+        else:
+            # minimax
+            depth = 1
+            eval = self.minimax(game, depth, -math.inf, math.inf, True)
+            if game.makeMove(eval["position"], self.letter):
+                return eval["position"]
+            else:
+                raise Exception("Something wrong with minimax output. It should be able to play the move.")
+            
         return
     
     # minimax algorithm with alpha beta pruning
-    def minimax(self):
-        return
+    def minimax(self, gameState, depth, alpha, beta, isMaximizing):
+        maxPlayer = self.letter
+        minPlayer = "X" if maxPlayer == "O" else "O"
+
+        if gameState.winner != None:
+            if gameState.winner == maxPlayer:
+                return {"score": 1*(gameState.getNumPosLeft() + 1), "position": None}
+            elif gameState.winner == minPlayer:
+                return {"score": -1*(gameState.getNumPosLeft() + 1), "position": None}
+        elif depth == 0:
+            # Get combinations that each player has on board
+            if isMaximizing:
+                return {"score": 1*gameState.getPlayerScore(maxPlayer), "position": None}
+            else:
+                return {"score": -1*gameState.getPlayerScore(minPlayer), "position": None}
+            pass
+        elif gameState.isBoardFull():
+            # A tie
+            return {"score": 0, "position": None}
+        
+        # TODO rest of minimax
+        if isMaximizing:
+            bestPlay = {"score": -math.inf, "position": None}
+            player = maxPlayer
+        else:
+            bestPlay = {"score": math.inf, "position": None}
+            player = minPlayer
+
+        # TODO iterate through every open pos that's a dist of 1-2 away
+
+        return bestPlay
     
     # heuristics that evaluate which move is the best
     def heuristics(self):
